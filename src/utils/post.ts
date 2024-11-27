9ed540d8178b33df1f3b27cf28bb6b9bd1eb2e82
Cassio Zen <cassiozen@gmail.com>
2024-11-26 21:44:26 -0600

    Removed categories

--- src/utils/post.ts
+++ src/utils/post.ts
@@ -1,15 +1,4 @@
 import { getCollection } from 'astro:content'
-import { CATEGORIES } from '@/data/categories'
-
-export const getCategories = async () => {
-	const posts = await getCollection('blog')
-	const categories = new Set(
-		posts.filter((post) => !post.data.draft).map((post) => post.data.category)
-	)
-	return Array.from(categories).sort((a, b) =>
-		CATEGORIES.indexOf(a) < CATEGORIES.indexOf(b) ? -1 : 1
-	)
-}
 
 export const getPosts = async (max?: number) => {
 	return (await getCollection('blog'))
@@ -40,11 +29,4 @@ export const getPostByTag = async (tag: string) => {
 		.filter((post) => {
 			return post.data.tags.some((postTag) => postTag.toLowerCase() === lowercaseTag)
 		})
-}
-
-export const filterPostsByCategory = async (category: string) => {
-	const posts = await getPosts()
-	return posts
-		.filter((post) => !post.data.draft)
-		.filter((post) => post.data.category.toLowerCase() === category)
 }
